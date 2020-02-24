f0f57025cfe1da75f3b6f35b441a9e225b672823
kishorereddy0909 <kishorereddy996@gmail.com>
2020-02-24 12:57:34 +0530

    [kishorereddy] Refactor. modified MoodAnalysisException.java to return exception type and message in MoodAnalysisException.java in src/main/java

--- src/main/java/MoodAnalysisException.java
+++ src/main/java/MoodAnalysisException.java
@@ -1,9 +1,16 @@
 public class MoodAnalysisException extends Exception {
-    enum Invalid{
+    public Invalid type;
+    private String message;
+    enum Invalid {
         NULL,
         EMPTY
     }
-    MoodAnalysisException(String exceptionMessage){
-        super(exceptionMessage);
+    MoodAnalysisException(Invalid type , String exceptionMessage){
+        message = exceptionMessage;
+        this.type = type;
+    }
+    @Override
+    public String toString() {
+        return message;
     }
 }
